aa6bdc0d2fe4116fb9790b8744d88cf85095508b
Nesswit <rishubil@gmail.com>
2021-05-22 04:56:44 +0900

    Fix CORS issue

--- data/src/flask/server.py
+++ data/src/flask/server.py
@@ -20,6 +20,12 @@ from text_processer import normalize_text, process_text
 app = Flask(__name__)
 
 
+@app.after_request
+def apply_caching(response):
+    response.headers['Access-Control-Allow-Origin'] = "*"
+    return response
+
+
 @app.route("/")
 def index():
     return redirect(url_for("text_inference"))
